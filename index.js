abba5cc349d17f463bd049169af00e7cf0b97dfa
Maverick901 <prithipalsinghpatwal@gmail.com>
2020-05-08 21:19:07 +0530

    feat: added subscribe method in createStore - store function passed to it as argument and pushed it to listeners array - return a function which removes the function passed as argument from listeners

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 function createStore() {
 
     let state
-
+    let listeners
     const getStore = () => state
 
+    const subscribe = (listener) => {
+        listeners.push(listener)
+
+        return () => {
+            listeners.filter((l) => l !== listener)
+        }
+    }
+
     return {
-        getStore
+        getStore,
+        subscribe
     }
 
 }
+
