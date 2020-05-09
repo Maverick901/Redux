c08ff15213524e03e9afd2f7cf96cd701ab3788c
Maverick901 <prithipalsinghpatwal@gmail.com>
2020-05-09 11:32:27 +0530

    feat: initialize createStore function and called its method

--- index.js
+++ index.js
@@ -2,12 +2,11 @@
 function createStore(reducer) {
 
     let state
-    let listeners
+    let listeners = []
     const getStore = () => state
 
     const subscribe = (listener) => {
         listeners.push(listener)
-
         return () => {
             listeners.filter((l) => l !== listener)
         }
@@ -29,10 +28,27 @@ function createStore(reducer) {
 }
 
 
-function todos(state, action) {
+function todos(state = [], action) {
 
-    if (action.type === "ADD_ITEM") {
+    if (action.type === "ADD_TODO") {
         return state.concat([action.todo])
     }
-    return state;
+    return state
 }
+
+
+const store = createStore(todos)
+
+store.subscribe(() => {
+    console.log("State : ", store.getStore())
+})
+
+store.dispatch({
+    type: "ADD_TODO",
+    todo: {
+        id: 0,
+        name: "Learn Redux",
+        completed: false
+    }
+})
+
